314c0b989e90c559ac7b21a22136a41965547c1c
Kanav Mahajan <kanavmahj@gmail.com>
2024-02-28 15:41:18 +0530

    Create multiof15usingwhile.c

---
+++ multiof15usingwhile.c
@@ -0,0 +1,17 @@
+/******************************************************************************
+Write a program to display multiplication table of 15 using while loop
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a=15;
+    int count=1;
+    while(count<=10){
+        printf("%d \n",count*a );
+        count++;
+    }
+
+    return 0;
+}
