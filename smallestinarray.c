b556691562fa8b1199c1207e5b4dc9e6d2c09fbb
Kanav Mahajan <kanavmahj@gmail.com>
2024-03-27 11:26:32 +0530

    Update smallestinarray.c

--- smallestinarray.c
+++ smallestinarray.c
@@ -1,14 +1,25 @@
 #include <stdio.h>    
      
 int main(){
-    
-    int a[4]= {121,214,342,4123};
+    int i, a[20], small, pos=0, n;
+    printf("Enter the no. of elements: ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+        small= a[0];
+    }    
+    for(i=1; i<n; i++){
+        if(a[i]<small){
+            small=a[i];
+            pos=i;
+        }
+    }    
     int length = sizeof(a)/sizeof(a[0]);    
     int min = a[0];    
     for (int i = 0; i < length; i++) {     
        if(a[i] < min)    
            min = a[i];    
     }      
-    printf("Smallest element: %d\n", min);    
+    printf("Smallest element: %d\n", small);    
     return 0;    
 }    
